d975a35abe6ded3912df53986b66e74db214e337
jose david cantero <jodacan10@gmail.com>
2025-11-06 18:27:38 -0500

    initial Next.js setup with linting and tooling

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+
+
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  eslintPluginUnicorn.configs.all,
+
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "node_modules/**",
+    "next.config.ts",
+    "package-lock.json",
+    ".cache/**",
+  ]),
+]);
